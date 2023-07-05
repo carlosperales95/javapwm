0fe6e3b4f278bedf0f12c54227bbba9ff37c1f4e
carlosperales95 <carlos.perales95@opendeusto.es>
2023-07-05 20:42:28 +0200

    database connection

--- src/main/java/com/sapokodes/javapwm/dataObjects/UsersRepo.java
+++ src/main/java/com/sapokodes/javapwm/dataObjects/UsersRepo.java
@@ -1,22 +1,21 @@
 package com.sapokodes.javapwm.dataObjects;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class UsersRepo {
         
     //private User user = new User();
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
+    
     public List<User> getUsers() {
-        List<User> usersList =  new ArrayList<>();
-
-        usersList.add(new User(1, "manololillo", "123pass", "manolo.manolillo@gmail.com" ));
-        usersList.add(new User(2, "uriente3", "mypass", "ursula.clienta@gmail.com" ));
-        usersList.add(new User(3, "paqui04", "1234", "paquillo2004@gmail.com" ));
-
-        return usersList;
+        return jdbcTemplate.query("SELECT * FROM users", BeanPropertyRowMapper.newInstance(User.class));
     }
 }
