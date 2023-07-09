713c030279e6373e8db7775cd95333374f233524
carlosperales95 <carlos.perales95@opendeusto.es>
2023-07-09 21:12:05 +0200

    unnecessary tags and imports

--- src/main/java/jpwm/credentials/CredentialRepository.java
+++ src/main/java/jpwm/credentials/CredentialRepository.java
@@ -3,9 +3,7 @@ package jpwm.credentials;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface CredentialRepository extends JpaRepository<Credential, Long> {
     List<Credential> findByUserId(Long userId);
 }
